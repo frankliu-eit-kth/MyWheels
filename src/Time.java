1fd098dde5362fcfb1727b4cab76462988055b86
frankliu-eit-kth <frankliu-eit-kth@users.noreply.github.com>
2018-11-20 12:17:24 +0100

    added time util

---
+++ src/Time.java
@@ -0,0 +1,10 @@
+
+public class Time {
+
+	public static void main(String args[]) {
+		//get current time milliseconds
+		long currentTime= System.currentTimeMillis();
+		System.out.println(currentTime);
+	}
+
+}
